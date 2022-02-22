2482013ceac6503b649e874c790aef190f1c4368
Ashokan Palanivel <86849575+Ashokan09@users.noreply.github.com>
2022-02-22 14:13:40 +0000

    Update simpleflask.py

--- simpleflask.py
+++ simpleflask.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():    
-    return render_template('Python/home.html')
+    #return render_template('Python/home.html')
+    return 'Ashokan Palanivel'
 
 if __name__ == "__main__":
     app.run(debug=True)
